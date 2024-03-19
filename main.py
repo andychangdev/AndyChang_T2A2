3f3e4adb25f72bdc7b0ea209dc75c51d9e81ccbf
andychangdev <andychang.qld@gmail.com>
2024-03-19 17:48:53 +1000

    feat: added global error handling and comments for main.py

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from flask import Flask
 from init import db, ma, bcrypt, jwt
 from marshmallow.exceptions import ValidationError
 
+
 def create_app():
     app = Flask(__name__)
 
     app.json.sort_keys = False
 
     # configs
-    app.config["SQLALCHEMY_DATABASE_URI"]=os.environ.get("DATABASE_URI")
-    app.config["JWT_SECRET_KEY"]=os.environ.get("JWT_SECRET_KEY")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
+    app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
 
     # connect libraries with flask app
     db.init_app(app)
@@ -18,17 +19,28 @@ def create_app():
     bcrypt.init_app(app)
     jwt.init_app(app)
 
+    @app.errorhandler(400)
+    def bad_request(error):
+        return {"Error": str(error)}, 400
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return {"Error": str(error)}, 404
+
     @app.errorhandler(ValidationError)
     def validation_error(error):
         return {"error": error.messages}, 400
 
     from controllers.cli_controller import db_commands
+
     app.register_blueprint(db_commands)
 
     from controllers.auth_controller import auth_bp
+
     app.register_blueprint(auth_bp)
 
     from controllers.itinerary_controller import itineraries_bp
+
     app.register_blueprint(itineraries_bp)
 
     return app
